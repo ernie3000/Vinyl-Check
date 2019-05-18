f7f348b5a98636cfb8c7b1c3d74cc26c3be3ada1
ernie3000 <50182266+ernie3000@users.noreply.github.com>
2019-05-18 03:22:20 +0200

    changed loop function

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,15 @@
  // Name, Height  and Type of Pokemon Array//
 var repository=[
  ['Stoutland', 2.9 ,['Intimidate' , 'Scrappy' , 'Sandrush']],
-
- ['Clefable', 1.8 , ['Cutecharm', 'Magicguard', 'Unaware']],
-
+ ['Clefable', 9.8 , ['Cutecharm', 'Magicguard', 'Unaware']],
  ['Gloom', 8.8 , ['Stench' , 'Chlorophyll']]
 ];
- // checks Array repository for length and starts loop//
-for (var i = 0; i < repository.length; i++){
-
-document.write ('<H2> Name:' + repository[i][0] + '</H2>');
-document.write ('<H3> Height:' + repository[i][1] + '</H3>');
+//object loop for repository, outputs every Pokemon //
+  Object.keys(repository).forEach(function(currentPokemon){
+document.write ('<p>'+ (repository[currentPokemon]) + '</p>')
 // checks if a Pokemon is above value 8 to write Text//
-if (repository[i][1] > 8){
+if (repository[currentPokemon][1] > 8){
     document.write ('Thats A Big One!!')
-}
-document.write ('<H3> Types:' + repository[i][2] + '</H3>')};
+    }});
 
- 
+   
